17034d7697600e180f9c3f01e41033420cdebf6d
CharanReddy404 <charanlucky6143@gmail.com>
2023-04-20 06:58:01 +0530

    api and redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { fetchDataFromApi } from './utils/api';
+import { getApiConfiguration } from './store/homeSlice';
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  const url = useSelector((state) => state.home.url);
+
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi('/movie/popular').then((res) => {
+      console.log(res);
+      dispatch(getApiConfiguration(res.data));
+    });
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className=''>
+      <h1 className='text-3xl font-bold underline'>
+        Hello world! {url?.total_pages}
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import { Provider } from 'react-redux';
+import App from './App';
+import { store } from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>
+);
